c5a68e4c1a18987ef58127f093a479f49d5bea00
Horbin <2240045751@qq.com>
2023-08-17 20:12:17 +0800

    Update: optimized architecture

--- src/application.rs
+++ src/application.rs
@@ -1,10 +1,13 @@
+mod system_tray;
+mod setting;
+
 use std::sync::mpsc;
 
 use slint::ComponentHandle;
 
-use crate::util::system_tray::SystemTray;
-use crate::module::searcher::SearchWindow;
-use crate::setting::SettingWindow;
+use crate::application::system_tray::SystemTray;
+use crate::application::setting::{Setting, SettingWindow};
+use crate::module::searcher::{Searcher, SearchWindow};
 
 pub enum AppMessage {
     Quit,
@@ -14,20 +17,29 @@ pub enum AppMessage {
 
 pub struct Application {
     _system_tray: SystemTray,
+    _setting: Setting,
+    _searcher: Searcher,
     _msg_sender:mpsc::Sender<AppMessage>,
 }
 
 impl Application {
-    pub fn new(setting: slint::Weak<SettingWindow>, searcher: slint::Weak<SearchWindow>) -> Self {
+    pub fn new() -> Self {
         let (_msg_sender, msg_reciever) = mpsc::channel();
-        let _system_tray = SystemTray::new(_msg_sender.clone());
 
+        let _system_tray = SystemTray::new(_msg_sender.clone());
+        let _searcher: Searcher = Searcher::new();
+        let _setting: Setting = Setting::new();
+
+        let setting_win = _setting.setting_win.as_weak();
+        let search_win = _searcher.search_win.as_weak();
         std::thread::spawn(move || {
-            app_loop(msg_reciever, setting, searcher);
+            app_loop(msg_reciever, setting_win, search_win);
         });
 
         Self {
             _system_tray,
+            _setting,
+            _searcher,
             _msg_sender,
         }
     }
@@ -37,7 +49,7 @@ impl Application {
     }
 }
 
-fn app_loop (msg_reciever: mpsc::Receiver<AppMessage>, setting: slint::Weak<SettingWindow>, searcher: slint::Weak<SearchWindow>) {
+fn app_loop (msg_reciever: mpsc::Receiver<AppMessage>, setting_win: slint::Weak<SettingWindow>, search_win: slint::Weak<SearchWindow>) {
     loop {
         match msg_reciever.try_recv() {
             Ok(AppMessage::Quit) => {
@@ -45,12 +57,12 @@ fn app_loop (msg_reciever: mpsc::Receiver<AppMessage>, setting: slint::Weak<Sett
                 break;
             },
             Ok(AppMessage::ShowSearch) => {
-                searcher.clone().upgrade_in_event_loop(move |setting| {
+                search_win.clone().upgrade_in_event_loop(move |setting| {
                     setting.show().unwrap();
                 }).unwrap();
             },
             Ok(AppMessage::ShowSetting) => {
-                setting.clone().upgrade_in_event_loop(move |setting| {
+                setting_win.clone().upgrade_in_event_loop(move |setting| {
                     setting.show().unwrap();
                 }).unwrap();
             },
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,13 @@
 mod application;
 mod module;
-mod setting;
 mod util;
 
-use slint::ComponentHandle;
 use i_slint_backend_selector;
 
-use crate::module::searcher::Searcher;
-use crate::setting::Setting;
 use crate::application::Application;
 
-
 fn main() {
-    let searcher = Searcher::new();
-    let setting = Setting::new();
-
-    let _app = Application::new(setting.setting_win.as_weak(), searcher.search_win.as_weak());
+    let _app = Application::new();
 
     i_slint_backend_selector::with_platform(|b| {
         b.set_event_loop_quit_on_last_window_closed(false);
