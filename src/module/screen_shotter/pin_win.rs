ae576dd714b1cf413117cd8d9902cb00123f808c
Horbin <2240045751@qq.com>
2023-12-15 19:12:29 +0800

    Update: try to add a toolbar, but failed.

--- src/module/screen_shotter/pin_win.rs
+++ src/module/screen_shotter/pin_win.rs
@@ -1,14 +1,16 @@
 use std::sync::mpsc::Sender;
 use std::thread;
-use muda::{Menu, MenuItem, ContextMenu, MenuEvent};
+// use muda::{Menu, MenuItem, ContextMenu, MenuEvent};
 use slint::Image;
 use i_slint_backend_winit::WinitWindowAccessor;
 use raw_window_handle::HasRawWindowHandle;
-// use chrono;
+use windows_sys::Win32::UI::WindowsAndMessaging;
 
+use super::toolbar::Toolbar;
 use super::Rect;
 
 pub struct PinWin {
+    _toolbar: Toolbar,
     pub pin_window: PinWindow,
 }
 
@@ -25,14 +27,24 @@ impl PinWin {
         pin_window.set_img_width(rect.width);
         pin_window.set_img_height(rect.height);
         
-        let save_item = MenuItem::new("保存", true, None);
-        let minimize_item = MenuItem::new("最小化", true, None);
-        let exit_item = MenuItem::new("退出", true, None);
-        let finish_item = MenuItem::new("完成", true, None);
-        let menu = Menu::with_items(&[&save_item, &minimize_item, &exit_item, &finish_item]).unwrap();
+        let toolbar = Toolbar::new();
+        let toolbar_win_width = toolbar.toolbar_window.get_win_width();
+        let pin_win_height = pin_window.get_win_height();
+        let pin_win_width = pin_window.get_win_width();
+        let now_pos = pin_window.window().position().to_logical(pin_window.window().scale_factor());
+        toolbar.toolbar_window.window().set_position(
+            slint::LogicalPosition::new(now_pos.x + pin_win_width - toolbar_win_width, now_pos.y + pin_win_height)
+        );
+
+        // let save_item = MenuItem::new("保存", true, None);
+        // let minimize_item = MenuItem::new("最小化", true, None);
+        // let exit_item = MenuItem::new("退出", true, None);
+        // let finish_item = MenuItem::new("完成", true, None);
+        // let menu = Menu::with_items(&[&save_item, &minimize_item, &exit_item, &finish_item]).unwrap();
 
         { // code for window move
             let pin_window_clone = pin_window.as_weak();
+            let toolbar_window_clone = toolbar.toolbar_window.as_weak();
             let move_sender_clone = move_sender.clone();
             pin_window.on_win_move(move |mut delta_x, mut delta_y| {
                 let pin_window_clone = pin_window_clone.unwrap();
@@ -55,72 +67,74 @@ impl PinWin {
                     }
                 }
                 if !is_stick_x || !is_stick_y {
-                    pin_window_clone.window().set_position(
-                        slint::LogicalPosition::new(now_pos.x + delta_x, now_pos.y + delta_y)
-                    );
+                    let change_pos_x = now_pos.x + delta_x;
+                    let change_pos_y = now_pos.y + delta_y;
+                    pin_window_clone.window().set_position(slint::LogicalPosition::new(change_pos_x, change_pos_y));
                     move_sender_clone.send(id).unwrap();
+                    
+                    let toolbar_win_clone = toolbar_window_clone.unwrap();
+                    let toolbar_win_width = toolbar_win_clone.get_win_width();
+                    let pin_win_height = pin_window_clone.get_win_height();
+                    let pin_win_width = pin_window_clone.get_win_width();
+                    toolbar_win_clone.window().set_position(
+                        slint::LogicalPosition::new(change_pos_x + pin_win_width - toolbar_win_width, change_pos_y + pin_win_height)
+                    );
                 }
             });
         }
 
-        { // code for right_menu
-            let pin_window_clone = pin_window.as_weak();
-            let pin_window = pin_window_clone.unwrap();
+        // { // code for right_menu
+        //     let pin_window_clone = pin_window.as_weak();
+        //     let pin_window = pin_window_clone.unwrap();
+        //     let toolbar_window_clone = toolbar.toolbar_window.as_weak();
+        //     pin_window.on_show_menu(move |mouse_x, mouse_y| {
+        //         let pin_window = pin_window_clone.unwrap();
+        //         pin_window.window().with_winit_window(|winit_win: &i_slint_backend_winit::winit::window::Window| {
+        //             let raw_window_handle = winit_win.raw_window_handle();
+        //             if let raw_window_handle::RawWindowHandle::Win32(win32_window_handle) = raw_window_handle {
+        //                 let hwnd = win32_window_handle.hwnd as isize;
+        //                 let position = muda::LogicalPosition { x: mouse_x, y: mouse_y };
+        //                 menu.show_context_menu_for_hwnd(hwnd as isize, Some(position.into()));
+        //             }
+        //         });
+        //     });
 
-            pin_window.on_show_menu(move |mouse_x, mouse_y| {
-                let pin_window = pin_window_clone.unwrap();
-                pin_window.window().with_winit_window(|winit_win: &i_slint_backend_winit::winit::window::Window| {
-                    let raw_window_handle = winit_win.raw_window_handle();
-                    if let raw_window_handle::RawWindowHandle::Win32(win32_window_handle) = raw_window_handle {
-                        let hwnd = win32_window_handle.hwnd as isize;
-                        let position = muda::LogicalPosition { x: mouse_x, y: mouse_y };
-                        menu.show_context_menu_for_hwnd(hwnd as isize, Some(position.into()));
-                    }
-                });
-            });
-        }
-
-        let save_item_id = save_item.id().clone();
-        let minimize_item_id = minimize_item.id().clone();
-        let exit_item_id = exit_item.id().clone();
-        let finish_item_id = finish_item.id().clone();
-        thread::spawn(move || {
-            loop {
-                if let Ok(event) = MenuEvent::receiver().recv() {
-                    match event.id {
-                        id if id == save_item_id => {
-                            println!("save_item");
-                        },
-                        id if id == minimize_item_id => {
-                            println!("minimize_item");
-                        },
-                        id if id == exit_item_id => {
-                            println!("exit_item");
-                        },
-                        id if id == finish_item_id => {
-                            println!("finish_item");
-                        },
-                        _ => {}
-                    }
-                }
-            }
-        });
+        //     let pin_window_clone = pin_window.as_weak();
+        //     let save_item_id = save_item.id().clone();
+        //     let minimize_item_id = minimize_item.id().clone();
+        //     let exit_item_id = exit_item.id().clone();
+        //     let finish_item_id = finish_item.id().clone();
+        //     thread::spawn(move || {
+        //         loop {
+        //             let pin_window_clone = pin_window_clone.clone();
+        //             if let Ok(event) = MenuEvent::receiver().recv() {
+        //                 if event.id == save_item_id {
+        //                     println!("save_item");
+        //                 } else if event.id == minimize_item_id {
+        //                     println!("minimize_item");
+        //                 } else if event.id == exit_item_id {
+        //                     println!("exit_item");
+        //                     slint::invoke_from_event_loop(
+        //                         move || {
+        //                             let pin_window = pin_window_clone.unwrap();
+        //                             pin_window.window().hide().unwrap();
+        //                         }
+        //                     ).unwrap();
+        //                 } else if event.id == finish_item_id {
+        //                     println!("finish_item");
+        //                 }
+        //             }
+        //         }
+        //     });
+        // }
 
         pin_window.show().unwrap();
+        toolbar.show();
         PinWin {
+            _toolbar: toolbar,
             pin_window,
         }
     }
-
-    // // TODO
-    // fn close_event() {
-    //     // emit sgn_close(this);
-    // }
-
-    // // TODO
-    // fn minimize() {
-    //     // setWindowState(Qt::WindowMinimized);
-    // }
 
     // // TODO
     // fn on_complete_screen() {
@@ -162,11 +176,12 @@ slint::slint! {
 
         in property <length> img_x;
         in property <length> img_y;
+        in-out property <int> zoom_factor: 10; // neet to be divided by ten
         in-out property <length> img_width;
         in-out property <length> img_height;
 
-        in-out property <length> win_width: img_width + win_border_width * 2;
-        in-out property <length> win_height: img_height + win_border_width * 2;
+        in-out property <length> win_width: (img_width * zoom_factor / 10) + win_border_width * 2;
+        in-out property <length> win_height: (img_height * zoom_factor / 10) + win_border_width * 2;
 
         in-out property <bool> is_stick_x;
         in-out property <bool> is_stick_y;
@@ -203,6 +218,7 @@ slint::slint! {
                         if (self.right_pressed) {return;} // right button alse trigger this event
                         root.win_move(self.mouse-x - self.pressed-x, self.mouse-y - self.pressed-y);
                     }
+
                     pointer-event(event) => {
                         if(event.button == PointerEventButton.right) {
                             self.right_pressed = true;
@@ -216,17 +232,18 @@ slint::slint! {
                         }
                     }
 
-                    // An alternative way to resize when there is no wheel event
-                    resize_touch_area := TouchArea {
-                        x: win_width - 10px;
-                        y: win_height - 10px;
-                        mouse-cursor: nwse-resize;
-                        moved => {
-                            win_width = win_width + self.mouse-x - self.pressed-x;
-                            win_height = win_height + self.mouse-y - self.pressed-y;
-                            if (win_width < 10px) {win_width = 10px};
-                            if (win_height < 10px) {win_height = 10px}
+                    scroll-event(event) => {
+                        debug(root.zoom_factor);
+                        if (event.delta-y > 0) {
+                            if (root.zoom_factor < 50) {
+                                root.zoom_factor = root.zoom_factor + 1;
+                            }
+                        } else if (event.delta-y < 0) {
+                            if (root.zoom_factor > 2) {
+                                root.zoom_factor = root.zoom_factor - 1;
+                            }
                         }
+                        return accept;
                     }
                 }
             }
