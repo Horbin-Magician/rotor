8d8acf9d46f4f03b008408b79113abffc2a0fa39
Horbin <2240045751@qq.com>
2024-03-24 00:02:05 +0800

    Update: log util.

--- src/core/util.rs
+++ src/core/util.rs
@@ -1,3 +1,4 @@
 pub mod file_util;
 pub mod sys_util;
 pub mod net_util;
+pub mod log_util;
---
+++ src/core/util/log_util.rs
@@ -0,0 +1,52 @@
+use std::{
+    fs::OpenOptions, env, fs,
+};
+use std::io::{self, Write};
+use chrono::Local;
+use windows_sys::Win32::UI::WindowsAndMessaging::{MessageBoxW, MB_OK};
+
+
+fn wide_null(s: &str) -> Vec<u16> {
+    s.encode_utf16().chain(std::iter::once(0)).collect()
+}
+
+fn write_log(message: &String) -> std::io::Result<()> {
+    let file_path = env::current_exe().unwrap().parent().unwrap().join("userdata");
+    if !file_path.exists() { fs::create_dir(&file_path)?; }
+    let log_path = file_path.join("log");
+
+    // Open the log file
+    let mut file = OpenOptions::new()
+        .append(true) // set it to append mode
+        .create(true) // create it if the file does not exist
+        .open(log_path)?;
+
+    writeln!(file, "{}", message)?;
+    Ok(())
+}
+
+pub fn log_error_without_log(content: &String) {
+    let title = wide_null("发生错误");
+    let content = wide_null(content.as_str());
+    unsafe {
+        MessageBoxW(0 as isize, content.as_ptr(), title.as_ptr(), MB_OK);
+    }
+}
+
+pub fn log_error(message: String)  {
+    let now_time = Local::now().format("%Y-%m-%d %H:%M:%S").to_string();
+    let error_content = format!("{now_time} [Error] {message}");
+    write_log(&error_content).unwrap_or_else(|err: io::Error| {
+        log_error_without_log(&format!("{:?}", err));
+    });
+    log_error_without_log(&error_content);
+}
+
+pub fn log_info(message: String)  {
+    let now_time = Local::now().format("%Y-%m-%d %H:%M:%S").to_string();
+    let error_content = format!("{now_time} [info] {message}");
+    write_log(&error_content).unwrap_or_else(|err: io::Error| {
+        log_error_without_log(&format!("{:?}", err));
+    });
+    println!("{}", error_content);
+}
