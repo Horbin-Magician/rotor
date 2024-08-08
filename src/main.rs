a145e2a84c2e8f8b03545a09e04bd10aafa3d7ab
Horbin <2240045751@qq.com>
2024-08-08 19:20:18 +0800

    update: del useless annotation

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,11 @@
 mod core;
 mod module;
 
-// use i_slint_backend_winit;
-// use i_slint_backend_winit::winit::platform::windows::WindowBuilderExtWindows;
-
 use crate::core::util::sys_util;
 use crate::core::util::file_util;
 use crate::core::application::Application;
 
-
 fn main() {
-    // let mut backend = i_slint_backend_winit::Backend::new().unwrap();
-    // backend.window_builder_hook = Some(Box::new(|builder| {
-    //     builder
-    //         .with_skip_taskbar(true)
-    // }));
-    // slint::platform::set_platform(Box::new(backend)).unwrap();
 
     if sys_util::run_as_admin() == true {return;}
 
