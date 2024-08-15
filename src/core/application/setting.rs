2de663bba3bc7b8823a6d4aa03417aeafe1d2b5e
Horbin <2240045751@qq.com>
2024-08-16 00:19:41 +0800

    update: update function with animation, proxy, and tips

--- src/core/application/setting.rs
+++ src/core/application/setting.rs
@@ -81,18 +81,40 @@ impl Setting {
         { // update
             let setting_win_clone = setting_win.as_weak();
             setting_win.on_check_update(move || {
-                // TODO deal with unknown version
-                let latest_version = net_util::Updater::global().lock().unwrap().get_latest_version().unwrap_or("unknown".to_string());
-                let current_version = option_env!("CARGO_PKG_VERSION").unwrap_or("unknown");
                 let setting_window = setting_win_clone.unwrap();
-                setting_window.set_current_version(current_version.into());
-                setting_window.set_latest_version(latest_version.into());
-                setting_window.set_update_modal_state(1);
+                setting_window.set_block(true);
+
+                let setting_win_clone = setting_win_clone.clone();
+                std::thread::spawn(move || {
+                    let latest_version = net_util::Updater::global().lock().unwrap().get_latest_version().unwrap_or("unknown".to_string());
+                    let current_version = option_env!("CARGO_PKG_VERSION").unwrap_or("unknown");
+
+                    setting_win_clone.upgrade_in_event_loop(move |setting_window| {
+                        setting_window.set_current_version(current_version.into());
+                        setting_window.set_latest_version(latest_version.into());
+                        setting_window.set_update_state(1);
+                        setting_window.set_block(false);
+                    }).unwrap();
+                });
             });
 
+            let setting_win_clone = setting_win.as_weak();
             setting_win.on_update(move || {
-                // TODO deal update failed
-                net_util::Updater::global().lock().unwrap().update_software().unwrap();
+                let setting_window = setting_win_clone.unwrap();
+                setting_window.set_block(true);
+                setting_window.set_update_state(0);
+
+                let setting_win_clone = setting_win_clone.clone();
+                std::thread::spawn(move || {
+                    net_util::Updater::global().lock().unwrap().update_software().unwrap_or_else(
+                        |e| {
+                            setting_win_clone.upgrade_in_event_loop(move |setting_window| {
+                                setting_window.set_block(false);
+                                setting_window.set_update_state(2);
+                            }).unwrap();
+                        }
+                    );
+                });
             });
         }
 
@@ -103,7 +125,7 @@ impl Setting {
 }
 
 slint::slint! {
-    import { CheckBox, StandardListView, Palette, Button } from "std-widgets.slint";
+    import { CheckBox, StandardListView, Palette, Button, ProgressIndicator } from "std-widgets.slint";
     import { BaseSettingPage, ScreenShotterSettingPage, SearchSettingPage } from "src/core/application/setting/UI/pages/pages.slint";
     import { SideBar } from "src/core/application/setting/UI/side_bar.slint";
     import { TitleBar } from "src/core/application/setting/UI/title_bar.slint";
@@ -127,11 +149,14 @@ slint::slint! {
         callback update();
 
         in property <string> version;
-        in-out property <int> update_modal_state: 0;
+        in-out property <int> update_state: 0;
         in-out property <string> current_version;
         in-out property <string> latest_version;
         in-out property <bool> power_boot;
         in-out property <int> theme;
+
+        in-out property <bool> block;
+        in-out property <float> progress: -1.0;
 
         touch := TouchArea {
             pointer-event(event) => {
@@ -150,118 +175,184 @@ slint::slint! {
                 border-radius: 14px;
                 clip: true;
                 
-                HorizontalLayout {
-                    side-bar := SideBar {
-                        model: [
-                            @tr("Menu" => "基础"),
-                            @tr("Menu" => "搜索"),
-                            @tr("Menu" => "截图"),
-                        ];
+                VerticalLayout {
+
+                    HorizontalLayout {
+                        side-bar := SideBar {
+                            model: [
+                                @tr("Menu" => "基础"),
+                                @tr("Menu" => "搜索"),
+                                @tr("Menu" => "截图"),
+                            ];
+                        }
+
+                        VerticalLayout {
+                            title_bar := TitleBar {}
+
+                            Rectangle {
+                                if(side-bar.current-item == 0) : 
+                                    BaseSettingPage {
+                                        version: version;
+                                        power_boot <=> root.power_boot;
+                                        theme <=> root.theme;
+                                        theme_changed(theme) => { root.theme_changed(theme); }
+                                        power_boot_changed(power_boot) => { root.power_boot_changed(power_boot); }
+                                        check_update() => { root.check_update(); }
+                                    }
+                                if(side-bar.current-item == 1) :
+                                    SearchSettingPage {}
+                                if(side-bar.current-item == 2) :
+                                    ScreenShotterSettingPage {}
+                            }
+                        }
                     }
 
-                    VerticalLayout {
-                        title_bar := TitleBar {}
+                    if block : ProgressIndicator {
+                        width: 100%;
+                        height: 4px;
+                        indeterminate: root.progress < 0.0 ? true : false;
+                        progress: root.progress < 0.0 ? 0.0 : root.progress;
+                    }
+                }
 
-                        Rectangle {
-                            if(side-bar.current-item == 0) : 
-                                BaseSettingPage {
-                                    version: version;
-                                    power_boot <=> root.power_boot;
-                                    theme <=> root.theme;
-                                    theme_changed(theme) => { root.theme_changed(theme); }
-                                    power_boot_changed(power_boot) => { root.power_boot_changed(power_boot); }
-                                    check_update() => { root.check_update(); }
-                                }
-                            if(side-bar.current-item == 1) :
-                                SearchSettingPage {}
-                            if(side-bar.current-item == 2) :
-                                ScreenShotterSettingPage {}
+
+            }
+        }
+
+        if (update_state > 0) : Rectangle {
+            height: root.height;
+            width: root.width;
+            background: Palette.background.with_alpha(0.5);
+
+            in-out property <int> update_state <=> root.update_state;
+            in-out property <string> current_version <=> root.current_version;
+            in-out property <string> latest_version <=> root.latest_version;
+            
+            TouchArea {
+                clicked() => { update_state = 0; }
+                Rectangle {
+                    height: 150px;
+                    width: 200px;
+                    background: Palette.background;
+                    border-width: 1px;
+                    border-radius: 14px;
+                    border-color: Palette.background.brighter(1);
+                    
+                    if (update_state == 1 && current_version == latest_version) : VerticalLayout {
+                        alignment: center;
+                        spacing: 5px;
+                        Text {
+                            horizontal-alignment: center;
+                            vertical-alignment: center;
+                            text: @tr("当前已是最新版本");
+                        }
+                        HorizontalLayout {
+                            padding-top: 10px;
+                            alignment: center;
+                            Button {
+                                height: 30px;
+                                width: 60px;
+                                text: @tr("确定");
+                                clicked() => { root.update_state = 0; }
+                            }
+                        }
+                    }
+
+                    if (update_state == 1 && current_version != latest_version && latest_version != "unknown"): VerticalLayout {
+                        alignment: center;
+                        spacing: 8px;
+                        padding-top: 10px;
+                        Text {
+                            horizontal-alignment: center;
+                            vertical-alignment: center;
+                            text: @tr("当前版本: {}", current_version);
+                        }
+                        Text {
+                            horizontal-alignment: center;
+                            vertical-alignment: center;
+                            text: @tr("最新版本: {}", latest_version);
+                        }
+                        Text {
+                            horizontal-alignment: center;
+                            vertical-alignment: center;
+                            text: @tr("是否更新至最新版本？");
+                        }
+
+                        HorizontalLayout {
+                            padding-top: 10px;
+                            alignment: center;
+                            spacing: 10px;
+                            Button {
+                                height: 28px;
+                                width: 60px;
+                                text: @tr("是");
+                                clicked() => { root.update(); }
+                            }
+                            Button {
+                                height: 28px;
+                                width: 60px;
+                                text: @tr("否");
+                                clicked() => { root.update_state = 0; }
+                            }
+                        }
+                    }
+
+                    if (update_state == 1 && latest_version == "unknown"): VerticalLayout {
+                        alignment: center;
+                        spacing: 5px;
+                        Text {
+                            horizontal-alignment: center;
+                            vertical-alignment: center;
+                            text: @tr("获取版本信息失败");
+                        }
+
+                        Text {
+                            horizontal-alignment: center;
+                            vertical-alignment: center;
+                            text: @tr("请检查网络环境");
+                        }
+
+                        HorizontalLayout {
+                            padding-top: 10px;
+                            alignment: center;
+                            spacing: 10px;
+                            Button {
+                                height: 28px;
+                                width: 60px;
+                                text: @tr("确定");
+                                clicked() => { root.update_state = 0; }
+                            }
+                        }
+                    }
+
+                    if (update_state == 2): VerticalLayout {
+                        alignment: center;
+                        spacing: 5px;
+                        Text {
+                            horizontal-alignment: center;
+                            vertical-alignment: center;
+                            text: @tr("更新失败");
+                        }
+                        Text {
+                            horizontal-alignment: center;
+                            vertical-alignment: center;
+                            text: @tr("请检查网络环境");
+                        }
+
+                        HorizontalLayout {
+                            padding-top: 10px;
+                            alignment: center;
+                            spacing: 10px;
+                            Button {
+                                height: 28px;
+                                width: 60px;
+                                text: @tr("确定");
+                                clicked() => { root.update_state = 0; }
+                            }
                         }
                     }
                 }
             }
         }
-
-        if (update_modal_state != 0) : 
-            Rectangle {
-                height: root.height;
-                width: root.width;
-                background: Palette.background.with_alpha(0.5);
-
-                in-out property <int> update_modal_state <=> root.update_modal_state;
-                in-out property <string> current_version <=> root.current_version;
-                in-out property <string> latest_version <=> root.latest_version;
-                
-                TouchArea {
-                    clicked() => { update_modal_state = 0; }
-                    Rectangle {
-                        height: 150px;
-                        width: 200px;
-                        background: Palette.background;
-                        border-width: 2px;
-                        border-radius: 5px;
-                        border-color: Palette.background.brighter(1);
-
-                        if (current_version == latest_version):
-                            VerticalLayout {
-                                alignment: center;
-                                spacing: 5px;
-                                Text {
-                                    horizontal-alignment: center;
-                                    vertical-alignment: center;
-                                    text: @tr("当前已是最新版本");
-                                }
-                                HorizontalLayout {
-                                    padding-top: 10px;
-                                    alignment: center;
-                                    Button {
-                                        height: 30px;
-                                        width: 60px;
-                                        text: @tr("确定");
-                                        clicked() => { root.update_modal_state = 0; }
-                                    }
-                                }
-                            }
-                        if (current_version != latest_version):
-                            VerticalLayout {
-                                alignment: center;
-                                spacing: 5px;
-                                Text {
-                                    horizontal-alignment: center;
-                                    vertical-alignment: center;
-                                    text: @tr("当前版本: {}", current_version);
-                                }
-                                Text {
-                                    horizontal-alignment: center;
-                                    vertical-alignment: center;
-                                    text: @tr("最新版本: {}", latest_version);
-                                }
-                                Text {
-                                    horizontal-alignment: center;
-                                    vertical-alignment: center;
-                                    text: @tr("请问是否更新至最新版本？");
-                                }
-
-                                HorizontalLayout {
-                                    padding-top: 10px;
-                                    alignment: center;
-                                    spacing: 10px;
-                                    Button {
-                                        height: 28px;
-                                        width: 60px;
-                                        text: @tr("是");
-                                        clicked() => { root.update(); }
-                                    }
-                                    Button {
-                                        height: 28px;
-                                        width: 60px;
-                                        text: @tr("否");
-                                        clicked() => { root.update_modal_state = 0; }
-                                    }
-                                }
-                            }
-                    }
-                }
-            }
     }
 }
