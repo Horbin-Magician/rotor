5214f238b193901f13de4822a4a7b77e3c96ca7a
Horbin <2240045751@qq.com>
2024-08-18 20:23:27 +0800

    update: setting local shortcuts

--- src/module/screen_shotter/pin_win.rs
+++ src/module/screen_shotter/pin_win.rs
@@ -235,16 +235,38 @@ impl PinWin {
 
         { // code for key press
             let pin_window_clone = pin_window.as_weak();
-            pin_window.on_key_release(move |event| {
+            pin_window.on_key_release(move |shortcut| {
+                //TODO: handle F1-F12
+                let mut text = shortcut.text.to_string();
+                if text == "\u{1b}" { text = "Esc".into(); } // escape
+                else if text == " " { text = "Space".into(); } // space
+                else if text == "\n" { text = "Enter".into(); } // enter
+                else if text.as_str() > "\u{1f}" && text.as_str() < "\u{7f}" { text = text.to_uppercase(); } // char
+                else { return; } // exclude other control string
+                
+                let mut shortcut_str = String::new();
+                if shortcut.modifiers.control { shortcut_str += "Ctrl+"; }
+                if shortcut.modifiers.shift { shortcut_str += "Shift+"; }
+                if shortcut.modifiers.meta { shortcut_str += "Win+"; }
+                if shortcut.modifiers.alt { shortcut_str += "Alt+"; }
+                else { shortcut_str += &text; }
+
                 let pin_window = pin_window_clone.unwrap();
-                if event.text == slint::SharedString::from(slint::platform::Key::Escape) { // close win
-                    pin_window.invoke_close();
-                } else if event.text == "h" { // hide win
-                    pin_window.invoke_hide();
-                } else if event.text == "s" { // save pic
+                let app_config = AppConfig::global().lock().unwrap();
+                let default = "unkown".to_string();
+                let shortcut_pinwin_save = app_config.get_shortcut("pinwin_save").unwrap_or(&default);
+                let shortcut_pinwin_close = app_config.get_shortcut("pinwin_close").unwrap_or(&default);
+                let shortcut_pinwin_copy = app_config.get_shortcut("pinwin_copy").unwrap_or(&default);
+                let shortcut_pinwin_hide = app_config.get_shortcut("pinwin_hide").unwrap_or(&default);
+
+                if shortcut_str.eq(shortcut_pinwin_save){
                     pin_window.invoke_save();
-                } else if event.text == slint::SharedString::from(slint::platform::Key::Return) { // copy pic and close
+                } else if shortcut_str == *shortcut_pinwin_close {
+                    pin_window.invoke_close();
+                } else if shortcut_str == *shortcut_pinwin_copy {
                     pin_window.invoke_copy();
+                } else if shortcut_str == *shortcut_pinwin_hide {
+                    pin_window.invoke_hide();
                 }
             });
         }
