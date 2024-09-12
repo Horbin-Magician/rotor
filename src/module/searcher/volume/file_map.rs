766d64cf610c8b6471ec122bafc63524c70edda8
Horbin <2240045751@qq.com>
2024-09-12 23:22:25 +0800

    fix: separate functions to file_map

---
+++ src/module/searcher/volume/file_map.rs
@@ -0,0 +1,240 @@
+use std::collections::{BTreeMap, HashMap};
+
+use std::error::Error;
+use std::io::{self, Write};
+use std::fs;
+use std::sync::mpsc::Receiver;
+
+use super::SearchResultItem;
+
+
+pub struct File {
+    pub parent_index: u64,
+    pub file_name: String,
+    pub filter: u32,
+    pub rank: i8,
+}
+
+#[derive(PartialEq, Eq, PartialOrd, Ord)]
+pub struct FileKey {
+    rank: i8,
+    pub index: u64,
+}
+
+// Calculates a 32bit value that is used to filter out many files before comparing their filenames
+pub fn make_filter(str: &str) -> u32 {
+    /*
+    Creates an address that is used to filter out strings that don't contain the queried characters
+    Explanation of the meaning of the single bits:
+    0-25 a-z
+    26 0-9
+    27 other ASCII
+    28 not in ASCII
+    */
+    let len = str.len();
+    if len == 0 { return 0;}
+    let mut address: u32 = 0;
+    let str_lower = str.to_lowercase();
+
+    for c in str_lower.chars() {
+        if c == '*' { 
+            continue; // Reserved for wildcard
+        } else if c.is_ascii_lowercase() {
+            address |= 1 << (c as u32 - 97);
+        } else if ('0'..='9').contains(&c) {
+            address |= 1 << 26;
+        } else if c < 127u8 as char {
+            address |= 1 << 27;
+        } else {
+            address |= 1 << 28;
+        }
+    }
+    address
+}
+
+pub struct FileMap {
+    pub start_usn: i64,
+    main_map: BTreeMap<FileKey, File>,
+    rank_map: HashMap<u64, i8, std::hash::BuildHasherDefault<fxhash::FxHasher>>,
+}
+
+// TODO 检查所有的pub是否必须pub
+impl FileMap {
+    pub fn new() -> FileMap{
+        FileMap {
+            start_usn: 0,
+            main_map: BTreeMap::new(),
+            rank_map: HashMap::default(),
+        }
+    }
+
+    // insert a file to the database by index, file name and parent index
+    pub fn insert(&mut self, index: u64, file_name: String, parent_index: u64) {
+        let filter = make_filter(&file_name);
+        let rank = Self::get_file_rank(&file_name);
+        self.insert_simple(index, File { parent_index, file_name, filter, rank });
+    }
+
+    // insert a file to the database by index and file struct
+    pub fn insert_simple(&mut self, index: u64, file: File) {
+        let key = FileKey {
+            rank: file.rank,
+            index,
+        };
+        self.rank_map.insert(index, file.rank);
+        self.main_map.insert(key, file);
+    }
+
+    // get a File by index
+    pub fn get(&self, index: &u64) -> Option<&File> {
+        if let Some(rank) = self.rank_map.get(index) {
+            let file_key = FileKey {
+                rank: *rank,
+                index: *index,
+            };
+            return self.main_map.get(&file_key);
+        }
+        None
+    }
+
+    pub fn remove(&mut self, index: &u64) {
+        if self.rank_map.contains_key(index) {
+            let file_key = FileKey {
+                rank: self.rank_map[index],
+                index: *index,
+            };
+            self.main_map.remove(&file_key);
+            self.rank_map.remove(index);
+        }
+    }
+
+    pub fn iter(&self) -> std::collections::btree_map::Iter<'_, FileKey, File> {
+        self.main_map.iter()
+    }
+
+    pub fn save(&self, path: &str) -> Result<(), std::io::Error> {
+        let mut save_file = fs::File::create(path)?;
+
+        let mut buf = Vec::new();
+        buf.write_all(&self.start_usn.to_be_bytes())?;
+        for (file_key, file) in self.iter() {
+            buf.write_all(&file_key.index.to_be_bytes())?;
+            buf.write_all(&file.parent_index.to_be_bytes())?;
+            buf.write_all(&(file.file_name.len() as u16).to_be_bytes())?;
+            buf.write_all(file.file_name.as_bytes())?;
+            buf.write_all(&file.filter.to_be_bytes())?;
+            buf.write_all(&file.rank.to_be_bytes())?;
+        }
+        let _ = save_file.write(&buf.to_vec());
+
+        Ok(())
+    }
+
+    pub fn read(&mut self, path: &str) -> Result<(), Box<dyn Error>> {
+        let file_data = fs::read(path)?;
+
+        if file_data.len() < 8 { return Err(io::Error::new(io::ErrorKind::InvalidData, "File data too short.").into()); }
+
+        self.start_usn = i64::from_be_bytes(file_data[0..8].try_into()?);
+        let mut ptr_index = 8;
+
+        while ptr_index < file_data.len() {
+            if ptr_index + 18 > file_data.len() { return Err(io::Error::new(io::ErrorKind::InvalidData, "File data size error.").into()); }
+            
+            let index = u64::from_be_bytes(file_data[ptr_index..ptr_index+8].try_into()?);
+            ptr_index += 8;
+            let parent_index = usize::from_be_bytes(file_data[ptr_index..ptr_index+8].try_into()?) as u64;
+            ptr_index += 8;
+            let file_name_len = u16::from_be_bytes(file_data[ptr_index..ptr_index+2].try_into()?) as u16;
+            ptr_index += 2;
+
+            if ptr_index + (file_name_len as usize) + 5 > file_data.len() { return Err(io::Error::new(io::ErrorKind::InvalidData, "File data size error.").into()); }
+
+            let file_name = String::from_utf8(file_data[ptr_index..(ptr_index + file_name_len as usize)].to_vec())?;
+            ptr_index += file_name_len as usize;
+            let filter = u32::from_be_bytes(file_data[ptr_index..ptr_index+4].try_into()?);
+            ptr_index += 4;
+            let rank = i8::from_be_bytes(file_data[ptr_index..ptr_index+1].try_into()?);
+            ptr_index += 1;
+            self.insert_simple(index, File { parent_index, file_name, filter, rank });
+        }
+
+        Ok(())
+    }
+
+    pub fn clear(&mut self) {
+        self.main_map.clear();
+        self.rank_map.clear();
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.main_map.is_empty()
+    }
+
+    // return rank by filename
+    fn get_file_rank(file_name: &str) -> i8 {
+        let mut rank: i8 = 0;
+
+        if file_name.to_lowercase().ends_with(".exe") { rank += 10; }
+        else if file_name.to_lowercase().ends_with(".lnk") { rank += 25; }
+
+        let tmp = 40i16 - file_name.len() as i16;
+        if tmp > 0 { rank += tmp as i8; }
+
+        rank
+    }
+
+    // Constructs a path for a directory
+    pub fn get_path(&self, index: &u64) -> Option<String> {
+        let mut path = String::new();
+        let mut loop_index = *index;
+        while loop_index != 0 {
+            let file_op = self.get(&loop_index);
+            if let Some(file) = file_op {
+                path.insert_str(0, (file.file_name.clone() + "\\").as_str());
+                loop_index = file.parent_index;
+            } else {
+                return None;
+            }
+        }
+        Some(path)
+    }
+
+    pub fn search(&self, query: &str, last_search_num: usize, stop_receiver: &Receiver<()>, batch: u8 ) -> (Option<Vec<SearchResultItem>>, usize) {
+        let mut result = Vec::new();
+        let mut find_num = 0;
+        let mut search_num: usize = 0;
+        let query_lower = query.to_lowercase();
+        let query_filter = make_filter(&query_lower);
+
+        let file_map_iter = self.iter().rev().skip(last_search_num);
+        for (_, file) in file_map_iter {
+            if stop_receiver.try_recv().is_ok() { return (None, 0); }
+            search_num += 1;
+            if (file.filter & query_filter) == query_filter && Self::match_str(&file.file_name, &query_lower) {
+                if let Some(path) = self.get_path(&file.parent_index) {
+                    result.push(SearchResultItem {
+                        path,
+                        file_name: file.file_name.clone(),
+                        rank: file.rank,
+                    });
+                    find_num += 1;
+                    if find_num >= batch { break; }
+                }
+            }
+        }
+
+        return (Some(result), search_num);
+    }
+
+    // return true if contain query
+    fn match_str(contain: &str, query_lower: &String) -> bool {
+        let lower_contain = contain.to_lowercase();
+        for s in query_lower.split('*') { // for wildcard
+            if !lower_contain.contains(s) {
+                return false;
+            }
+        }
+        true
+    }
+}
