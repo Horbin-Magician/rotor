0b368f11dd483f148a0cd07ae64f416615c2db62
Horbin <2240045751@qq.com>
2024-09-23 19:24:48 +0800

    update: normal log_error do not show msgBox

--- src/util/log_util.rs
+++ src/util/log_util.rs
@@ -15,6 +15,14 @@ fn wide_null(s: &str) -> Vec<u16> {
     s.encode_utf16().chain(std::iter::once(0)).collect()
 }
 
+fn log_error_with_message_box(content: &str) {
+    let title = wide_null("Error");
+    let content = wide_null(content);
+    unsafe {
+        MessageBoxW(HWND(std::ptr::null_mut()), PCWSTR(content.as_ptr()), PCWSTR(title.as_ptr()), MB_OK);
+    }
+}
+
 fn write_log(message: &String) -> std::io::Result<()> {
     let file_path = file_util::get_userdata_path();
     if !file_path.exists() { fs::create_dir(&file_path)?; }
@@ -30,20 +38,11 @@ fn write_log(message: &String) -> std::io::Result<()> {
     Ok(())
 }
 
-fn log_error_without_log(content: &str) {
-    let title = wide_null("Error");
-    let content = wide_null(content);
-    unsafe {
-        MessageBoxW(HWND(std::ptr::null_mut()), PCWSTR(content.as_ptr()), PCWSTR(title.as_ptr()), MB_OK);
-    }
-}
-
 pub fn log_error(message: String)  {
     let now_time = Local::now().format("%Y-%m-%d %H:%M:%S").to_string();
     let error_content = format!("{now_time} [Error] {message}");
     write_log(&error_content)
-        .unwrap_or_else(|e: io::Error| log_error_without_log(&format!("{:?}", e)));
-    log_error_without_log(&error_content);
+        .unwrap_or_else(|e: io::Error| log_error_with_message_box(&format!("{:?}", e)));
 }
 
 #[allow(dead_code)]
@@ -51,6 +50,6 @@ pub fn log_info(message: String)  {
     let now_time = Local::now().format("%Y-%m-%d %H:%M:%S").to_string();
     let error_content = format!("{now_time} [info] {message}");
     write_log(&error_content)
-        .unwrap_or_else(|e: io::Error| log_error_without_log(&format!("{:?}", e)));
+        .unwrap_or_else(|e: io::Error| log_error_with_message_box(&format!("{:?}", e)));
     println!("{}", error_content);
 }
