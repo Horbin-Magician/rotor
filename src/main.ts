e1debdb909cab05343c8d787ce7c65d20223a8e2
Horbin <2101112165@stu.pku.edu.cn>
2025-07-17 00:00:50 +0800

    chore: organize the files

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 import naive from 'naive-ui'
 
 const Setting = () => import('./pages/Setting.vue')
-const SSMask = () => import('./pages/SSMask.vue')
-const SSPin = () => import('./pages/SSPin.vue')
+const ScreenShotterMask = () => import('./pages/ScreenShotter/Mask.vue')
+const ScreenShotterPin = () => import('./pages/ScreenShotter/Pin.vue')
 
 const routes = [
   { path: '/', component: Setting },
-  { path: '/ssmask', component: SSMask },
-  { path: '/sspin', component: SSPin },
+  { path: '/ScreenShotter/Mask', component: ScreenShotterMask },
+  { path: '/ScreenShotter/Pin', component: ScreenShotterPin },
 ]
 
 const router = createRouter({
