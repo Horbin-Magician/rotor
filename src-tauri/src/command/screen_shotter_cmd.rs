78cb9e28fa00cd9ffeb24b13d3d6cc1e84c682e0
Horbin <2240045751@qq.com>
2025-07-18 13:00:19 +0800

    chore: code style

--- src-tauri/src/command/screen_shotter_cmd.rs
+++ src-tauri/src/command/screen_shotter_cmd.rs
@@ -9,27 +9,37 @@ pub async fn capture_screen(webview_window: tauri::WebviewWindow) -> tauri::ipc:
             .and_then(|s| s.as_any().downcast_ref::<ScreenShotter>())
             .map(|screenshot| screenshot.masks.clone())
     };
-    
+
     let image = if let Some(masks_arc) = masks_arc {
         let masks = masks_arc.lock().await;
         masks.get(webview_window.label()).cloned()
-    } else { None };
+    } else {
+        None
+    };
     tauri::ipc::Response::new(image.unwrap_or_default())
 }
 
 #[tauri::command]
-pub fn new_pin(x: String, y: String, width: String, height: String, webview_window: tauri::WebviewWindow) {
+pub fn new_pin(
+    x: String,
+    y: String,
+    width: String,
+    height: String,
+    webview_window: tauri::WebviewWindow,
+) {
     let mut app = Application::global().lock().unwrap();
     let screenshot = app.get_module("screenshot");
     if let Some(s) = screenshot {
         if let Some(screenshot) = s.as_any_mut().downcast_mut::<ScreenShotter>() {
-            screenshot.new_pin(
-                x.parse::<f64>().unwrap(),
-                y.parse::<f64>().unwrap(),
-                width.parse::<f64>().unwrap(),
-                height.parse::<f64>().unwrap(),
-                webview_window
-            ).unwrap();
+            screenshot
+                .new_pin(
+                    x.parse::<f64>().unwrap(),
+                    y.parse::<f64>().unwrap(),
+                    width.parse::<f64>().unwrap(),
+                    height.parse::<f64>().unwrap(),
+                    webview_window,
+                )
+                .unwrap();
         }
     }
 }
