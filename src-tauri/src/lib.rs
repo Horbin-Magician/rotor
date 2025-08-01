bd16351067e87d9760d35748c92f1f4224509007
Horbin <2101112165@stu.pku.edu.cn>
2025-08-01 19:48:05 +0800

    chore: set LevelFilter::Debug

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -12,22 +12,13 @@ use core::application;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     let app = tauri::Builder::default()
-        .plugin(tauri_plugin_log::Builder::new().build())
+        .plugin(tauri_plugin_log::Builder::new().level(log::LevelFilter::Debug).build())
         .plugin(tauri_plugin_dialog::init())
-        .plugin(tauri_plugin_autostart::init(
-            MacosLauncher::LaunchAgent,
-            None,
-        ))
+        .plugin(tauri_plugin_autostart::init(MacosLauncher::LaunchAgent,None))
         .plugin(tauri_plugin_clipboard_manager::init())
-        .plugin(tauri_plugin_single_instance::init(|_app, _argv, _cwd| {
-            // You can handle the single instance event here if needed
-        }))
+        .plugin(tauri_plugin_single_instance::init(|_app, _argv, _cwd| {}))
         .plugin(tauri_plugin_opener::init())
-        .plugin(
-            tauri_plugin_global_shortcut::Builder::new()
-                .with_handler(application::handle_global_hotkey_event)
-                .build(),
-        )
+        .plugin(tauri_plugin_global_shortcut::Builder::new().with_handler(application::handle_global_hotkey_event).build())
         .invoke_handler(tauri::generate_handler![
             config_cmd::get_cfg,
             config_cmd::get_all_cfg,
