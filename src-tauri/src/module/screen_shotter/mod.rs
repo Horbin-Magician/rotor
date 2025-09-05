57ac3c93d3e651d060c9ee08b7d6bcf5a3c76a5b
Horbin <2240045751@qq.com>
2025-09-05 11:27:29 +0800

    fix: update xcap dependency to version 0.7.0 and optimize monitor image capture logic

--- src-tauri/src/module/screen_shotter/mod.rs
+++ src-tauri/src/module/screen_shotter/mod.rs
@@ -52,24 +52,27 @@ impl Module for ScreenShotter {
         // Capture screen
         #[cfg(target_os = "windows")]
         {
-            // TODO optimize performance
-            let masks_clone = Arc::clone(&self.masks);
-            tauri::async_runtime::spawn(async move {
-                for monitor in Monitor::all().unwrap() {
+            for monitor in Monitor::all()? {
+                let masks_clone = Arc::clone(&self.masks);
+                let pos_x = monitor.x()?;
+                let pox_y = monitor.y()?;
+                tauri::async_runtime::spawn(async move {
+                    let monitor = Monitor::from_point(pos_x, pox_y).unwrap();
                     let label = format!("ssmask-{}", monitor.id().unwrap());
                     let mut masks = masks_clone.lock().unwrap();
                     if let Ok(img) = monitor.capture_image() {
                         masks.insert(label.clone(), img);
                     }
-                }
-            });
+                });
+            }
         }
+
         #[cfg(target_os = "macos")]
         {
             for monitor in Monitor::all()? {
                 let masks_clone = Arc::clone(&self.masks);
-                let label = format!("ssmask-{}", monitor.id()?);
                 tauri::async_runtime::spawn(async move {
+                    let label = format!("ssmask-{}", monitor.id().unwrap());
                     let mut masks = masks_clone.lock().unwrap();
                     if let Ok(img) = monitor.capture_image() {
                         masks.insert(label.clone(), img);
